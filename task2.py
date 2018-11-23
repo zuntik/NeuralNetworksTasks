3ee490325f619b97207ed12134e5c144b73c8eb0
javierbezares <javierbezares2@gmail.com>
2018-11-23 20:18:31 +0100

    Added one layer network

--- task2.py
+++ task2.py
@@ -4,6 +4,7 @@ import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.random as rd
 
 # Data
 # Character a...z
@@ -57,8 +58,8 @@ print("\nWe have 1559 characters and classes, we have 300 attributes")
 print(isolate_test.shape)
 
 # To train the network we have to convert the classes in one out of 26 vectors
-# We pass the previos data and write the dimension of the new vector (26 values per vector)
-isolate_data_class_binary = tf.one_hot(isolate_data_class, 26)
+isolate_data_class_binary = np.eye(26)[isolate_data_class-1]
+isolate_test_class_binary = np.eye(26)[isolate_test_class-1]
 # Now we have 1559 binary vectors each one with a range from 1 to 26
 print(isolate_data_class_binary)
 
@@ -66,5 +67,71 @@ print(isolate_data_class_binary)
 #--Normalize data--
 # normalize_data = (data - min(data)) / (max(data) - min(data))
 isolate_data = ((isolate_data +1) / 2)
+isolate_test = ((isolate_test +1) / 2)
 
 # look model.fit documentation, you can put bacht dim there
+#300 filas
+#programar 1 neurona (entrada hiden output)
+
+# mini bacth of size 40, with 300 features
+# 300/40 = 7,5 ~ 7 neurons input
+
+
+# Initial weight first layer. Connect 7 neurons (first layer) to 
+# la matriz de pesos en el nxm siendo "n" el numero de datos(o neuronas) de entrada y "m" el de neuronas de salida
+# el bias tiene que ser del tamaño de la salida ( es decir "m")
+# al principio tendriamos (300, 7) 300 datos de entrada para 7 neuronas de salida
+W = tf.Variable(rd.randn(300,26),trainable=True)
+b = tf.Variable(np.zeros(26),trainable=True)
+
+# El input de la primera capa, tenemos 300 col, es decir cada "caracter" tiene 300 dimensiones o caracteristicas
+# La dimenson de las filas se inicializa en None pero si queremos recibir mas de un caracter introduciriamos ese numero en vez de None
+x = tf.placeholder(shape=(None,300),dtype=tf.float64)
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# Placeholder
+y_ = tf.placeholder(shape=(None,26),dtype=tf.float64)
+# Este es el error medio por caracter
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
+
+# We create a variable for later initialize all variables
+init = tf.initialize_all_variables()
+
+# Session is responsible of building the inital graph
+# We create the graph  
+sess = tf.Session()
+
+# Re init variables to start from scratch
+sess.run(init)
+
+# Create lists for monitoring
+test_error_list = []
+train_error_list = []
+
+for k in range(20):
+    # Compute a gradient step
+    sess.run(train_step, feed_dict={x:isolate_data, y_:isolate_data_class_binary} )
+    
+    # Compute the losses on training and testing sets for monitoring
+    train_err = sess.run(cross_entropy, feed_dict={x:isolate_data, y_:isolate_data_class_binary})
+    test_err = sess.run(cross_entropy, feed_dict={x:isolate_test, y_:isolate_test_class_binary})
+    test_error_list.append(test_err)
+    train_error_list.append(train_err)
+
+
+
+fig,ax = plt.subplots(1)
+ax.plot(train_error_list, color='blue', label='training', lw=2)
+ax.plot(test_error_list, color='green', label='testing', lw=2)
+ax.set_xlabel('Training epoch')
+ax.set_ylabel('Cross-entropy')
+plt.legend()
+plt.show()
+
+
+
+
