869f0ddf898a6c49b67caf8e696a5f779744d69b
javierbezares <javierbezares2@gmail.com>
2018-11-24 20:26:50 +0100

    Delete old task2

--- task2.py
+++ task2.py
@@ -1,49 +1,25 @@
 import tensorflow as tf
-import keras
-
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as rd
+import nn18_ex2_load
 
-# Data
-# Character a...z
-# 26 classes
+isolate_data, isolate_data_class, isolate_test, isolate_test_class = nn18_ex2_load.load_isolet()
 
-def load_isolet():
-  # Loads the isolet dataset
-  # Returns:
-  # X....feature vectors (training set), X[i,:] is the i-th example
-  # C....target classes
-  # X_tst...feature vectors (test set)
-  # C_tst...classes (test set)
-  
-  import pickle as pckl  # to load dataset
-  import pylab as pl     # for graphics
-  #from numpy import *    
+# Recieves a placeholder x, and the number of inputs and outputs
+# returns the W matrix, the bias and the sigmoid fuction
+def layer(x, n_input, n_output):
+  W = tf.Variable(rd.randn(n_input,n_output),trainable=True)
+  b = tf.Variable(np.zeros(n_output),trainable=True)
+  y = tf.nn.sigmoid(tf.matmul(x,W) + b)
+  return (W, b, y)
 
-  pl.close('all')   # closes all previous figures
-
-  # Load dataset
-  file_in = open('isolet_crop_train.pkl','rb')
-  isolet_data = pckl.load(file_in) # Python 3
-  #isolet_data = pckl.load(file_in, encoding='bytes') # Python 3
-  file_in.close()
-  X = isolet_data[0]   # input vectors X[i,:] is i-th example
-  C = isolet_data[1]   # c.lasses C[i] is class of i-th example
-
-  file_in = open('isolet_crop_test.pkl','rb')
-  isolet_test = pckl.load(file_in) # Python 3
-  file_in.close()
-
-  X_tst = isolet_test[0]   # input vectors X[i,:] is i-th example
-  C_tst = isolet_test[1]   # classes C[i] is class of i-th example
-
-  return (X, C, X_tst, C_tst)
-
-isolate_data, isolate_data_class, isolate_test, isolate_test_class = load_isolet()
-
-
+# Is like the function layer but we return a softmax function instead of a sigmoid
+def last_layer(x, n_input, n_output):
+  W = tf.Variable(rd.randn(n_input,n_output),trainable=True)
+  b = tf.Variable(np.zeros(n_output),trainable=True)
+  z = tf.nn.softmax(tf.matmul(x,W) + b)
+  return (W, b, z)
 
 print("\n--DATA SET--")
 print("\nWe have 6238 character and classes (each character belongs to a class)")
@@ -69,68 +45,93 @@ print(isolate_data_class_binary)
 isolate_data = ((isolate_data +1) / 2)
 isolate_test = ((isolate_test +1) / 2)
 
-# look model.fit documentation, you can put bacht dim there
-#300 filas
-#programar 1 neurona (entrada hiden output)
 
-# mini bacth of size 40, with 300 features
-# 300/40 = 7,5 ~ 7 neurons input
+n_input = 300
+num_neuron = 100
+
+x = tf.placeholder(shape=(None, 300),dtype=tf.float64)
+
+#-- 2 LAYERS --
+W_hid , b_hid, y = layer(x, 300, num_neuron)
+W_out , b_out, z = last_layer(y, num_neuron, 26)
+
+#-- 3 LAYERS --
+#W_hid , b_hid, y = layer(x, 300, num_neuron)
+#W_hid2 , b_hid2, y2 = layer(y, num_neuron, num_neuron)
+#W_out , b_out, z = last_layer(y2, num_neuron, 26)
+
+#-- 4 LAYERS --
+#W_hid , b_hid, y = layer(x, 300, num_neuron)
+#W_hid2 , b_hid2, y2 = layer(y, num_neuron, num_neuron)
+#W_hid3 , b_hid3, y3 = layer(y2, num_neuron, num_neuron)
+#W_out , b_out, z = last_layer(y3, num_neuron, 26)
 
 
-# Initial weight first layer. Connect 7 neurons (first layer) to 
-# la matriz de pesos en el nxm siendo "n" el numero de datos(o neuronas) de entrada y "m" el de neuronas de salida
-# el bias tiene que ser del tamaño de la salida ( es decir "m")
-# al principio tendriamos (300, 7) 300 datos de entrada para 7 neuronas de salida
-W = tf.Variable(rd.randn(300,26),trainable=True)
-b = tf.Variable(np.zeros(26),trainable=True)
+z_ = tf.placeholder(shape=(None,26),dtype=tf.float64)
 
-# El input de la primera capa, tenemos 300 col, es decir cada "caracter" tiene 300 dimensiones o caracteristicas
-# La dimenson de las filas se inicializa en None pero si queremos recibir mas de un caracter introduciriamos ese numero en vez de None
-x = tf.placeholder(shape=(None,300),dtype=tf.float64)
-y = tf.nn.softmax(tf.matmul(x,W) + b)
-
-# Placeholder
-y_ = tf.placeholder(shape=(None,26),dtype=tf.float64)
-# Este es el error medio por caracter
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(z_ * tf.log(z), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(z,1), tf.argmax(z_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
 
 # We create a variable for later initialize all variables
 init = tf.initialize_all_variables()
-
-# Session is responsible of building the inital graph
-# We create the graph  
+# Create the session
 sess = tf.Session()
-
 # Re init variables to start from scratch
 sess.run(init)
 
-# Create lists for monitoring
-test_error_list = []
-train_error_list = []
+# Create some list to monitor how error decreases
+test_loss_list = []
+train_loss_list = []
 
-for k in range(20):
-    # Compute a gradient step
-    sess.run(train_step, feed_dict={x:isolate_data, y_:isolate_data_class_binary} )
+test_acc_list = []
+train_acc_list = []
+
+# Create minibtaches to train faster
+k_batch = 40
+X_batch_list = np.array_split(isolate_data,k_batch)
+labels_batch_list = np.array_split(isolate_data_class_binary,k_batch)
+
+for k in range(500):
+    # Run gradient steps over each minibatch
+    for x_minibatch,labels_minibatch in zip(X_batch_list,labels_batch_list):
+        sess.run(train_step, feed_dict={x: x_minibatch, z_:labels_minibatch})
+        
+    # Compute the errors over the whole dataset
+    train_loss = sess.run(cross_entropy, feed_dict={x:isolate_data, z_:isolate_data_class_binary})
+    test_loss = sess.run(cross_entropy, feed_dict={x:isolate_test, z_:isolate_test_class_binary})
     
-    # Compute the losses on training and testing sets for monitoring
-    train_err = sess.run(cross_entropy, feed_dict={x:isolate_data, y_:isolate_data_class_binary})
-    test_err = sess.run(cross_entropy, feed_dict={x:isolate_test, y_:isolate_test_class_binary})
-    test_error_list.append(test_err)
-    train_error_list.append(train_err)
+    # Compute the acc over the whole dataset
+    train_acc = sess.run(accuracy, feed_dict={x:isolate_data, z_:isolate_data_class_binary})
+    test_acc = sess.run(accuracy, feed_dict={x:isolate_test, z_:isolate_test_class_binary})
+    
+    # Put it into the lists
+    test_loss_list.append(test_loss)
+    train_loss_list.append(train_loss)
+    test_acc_list.append(test_acc)
+    train_acc_list.append(train_acc)
+    
+    if np.mod(k,50) == 0:
+        print('iteration {} test accuracy: {:.3f}'.format(k+1,test_acc))
 
 
 
-fig,ax = plt.subplots(1)
-ax.plot(train_error_list, color='blue', label='training', lw=2)
-ax.plot(test_error_list, color='green', label='testing', lw=2)
-ax.set_xlabel('Training epoch')
-ax.set_ylabel('Cross-entropy')
-plt.legend()
+
+fig,ax_list = plt.subplots(1,2)
+ax_list[0].plot(train_loss_list, color='blue', label='training', lw=2)
+ax_list[0].plot(test_loss_list, color='green', label='testing', lw=2)
+ax_list[1].plot(train_acc_list, color='blue', label='training', lw=2)
+ax_list[1].plot(test_acc_list, color='green', label='testing', lw=2)
+
+ax_list[0].set_xlabel('training iterations')
+ax_list[1].set_xlabel('training iterations')
+ax_list[0].set_ylabel('Cross-entropy')
+ax_list[1].set_ylabel('Accuracy')
+plt.legend(loc=2)
 plt.show()
+
+# checkpoont , save summary
 
 
 
