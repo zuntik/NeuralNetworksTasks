a19f31a45251c776b59f6fb12023e329d9dfba9a
thomas <thomasdpb@gmail.com>
2019-01-14 21:41:05 +0100

    nearly there

--- NN_task5/task5.py
+++ NN_task5/task5.py
@@ -18,36 +18,22 @@ y_dim = 7
 #sequence_length = 20
 num_train, num_valid, num_test = 5000, 500, 500
 
-#cell_type = 'simple'
-#cell_type = 'gru'
-cell_type = 'lstm'
 # our number of hidden units is 14 not 20
 num_hidden = 20
 
 # the batch size for the stochastic gradient descent is not defined, for now it
-# can be 10% of the total, 50
+# can be 1% of the total, 50, which will allow 100 changes to the weights per
+# epoch
 batch_size = 50
-# learning rate is not defined, for now it can be original
-learning_rate = 0.01
-# max epoch is not defined, for now it can be original
+learning_rate = 0.1
 # max epoch is necessary because the error may never be small enough on the
 # validadtion samples
-max_epoch = 200
+max_epoch = 300
 
 # ----------------------------------------------------------------------
 
 # Data generation
 
-
-## Generate delayed XOR samples
-#X_train, y_train = generate_data(num_train, sequence_length)
-#sl_train = sequence_length * np.ones(num_train) # NEW
-#
-#X_valid, y_valid = generate_data(num_valid, sequence_length)
-#sl_valid = sequence_length * np.ones(num_valid) # NEW
-#
-#X_test, y_test = generate_data(num_test, sequence_length)
-#sl_test = sequence_length * np.ones(num_test) # NEW
 
 words = [ make_embedded_reber() for i in range(num_train + num_valid + num_test) ]
 max_len = len(max(words,key=len))
@@ -68,21 +54,6 @@ y_test = np.array( next_chars_one_hot[-num_test:] )
 sl_test = sl[-num_test:]
 
 
-# not related
-# # Crop data
-# # Artificially define variable sequence lengths
-# # for demo-purposes
-# for i in range(num_train):
-#     ll = 10+random.randint(0,sequence_length-10)
-#     sl_train[i] = ll
-# 
-# for i in range(num_valid):
-#     ll = 10+random.randint(0,sequence_length-10)
-#     sl_valid[i] = ll
-# 
-# for i in range(num_test):
-#     ll = 10+random.randint(0,sequence_length-10)
-#     sl_test[i] = ll
 
 # placeholder for the sequence length of the examples
 seq_length = tf.placeholder(tf.int32, [None])
@@ -90,32 +61,21 @@ seq_length = tf.placeholder(tf.int32, [None])
 # input tensor shape: number of examples, input length, dimensionality of each input
 # at every time step, one bit is shown to the network
 # we one hotted the data, so the output has 7
-# X = tf.placeholder(tf.float32, [None, sequence_length, 1])
 X = tf.placeholder(tf.float32, [None, max_len, 7])
 
 # output tensor shape: number of examples, dimensionality of each output
 # Binary output at end of sequence
 # in our exercise we have a "many to many" scenario and each output 
 # is a vector
-# y = tf.placeholder(tf.float32, [None, 1])
 y = tf.placeholder(tf.float32, [None, max_len, 7])
 
-# define recurrent layer
-if cell_type == 'simple':
-  cell = tf.nn.rnn_cell.BasicRNNCell(num_hidden)
-  # cell = tf.keras.layers.SimpleRNNCell(num_hidden) #alternative
-elif cell_type == 'lstm':
-  cell = tf.nn.rnn_cell.LSTMCell(num_hidden)
-elif cell_type == 'gru':
-  cell = tf.nn.rnn_cell.GRUCell(num_hidden)
-else:
-  raise ValueError('bad cell type.')
+cell = tf.nn.rnn_cell.LSTMCell(num_hidden)
 # Cells are one fully connected recurrent layer with num_hidden neurons
 # Activation function can be defined as second argument.
 # Standard activation function is tanh for BasicRNN and GRU
 
 # we want to a trainable output layer 
-cell = tensorflow.contrib.rnn.OutputProjectionWrapper(cell, y_dim)
+cell = tf.contrib.rnn.OutputProjectionWrapper(cell, y_dim)
 
 # only use outputs, ignore states
 outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32, sequence_length=seq_length) # NEW
@@ -125,33 +85,21 @@ outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32, sequence_length=s
 # Returns:
 # outputs: The RNN output Tensor shaped: [batch_size, max_time, cell.output_size].
 
-# the following extraction is useless for  our case because we want the output
-# for every time step
-# get the unit outputs at the last time step
-#last_outputs = outputs[:,-1,:]
-
-# add output neuron
-#y_dim = int(y.shape[1])
-#w = tf.Variable(tf.truncated_normal([num_hidden, y_dim]))
-#b = tf.Variable(tf.constant(.1, shape=[y_dim]))
-
-#y_pred = tf.nn.xw_plus_b(last_outputs, w, b)
-# Matrix multiplication with bias
-
-
 
 # define loss, minimizer and error
 # the cross_entropy will have to be redefined to include error
 #  for all of the time steps
 #cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y)
-cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y)
+cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=outputs, labels=y))
 
 
 
 # we still want to reduce cross_entropy which by now has been redefined
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
-mistakes = tf.not_equal(y, tf.maximum(tf.sign(y_pred), 0))
+
+mistakes = tf.not_equal(y, tf.maximum(tf.sign(outputs), 0))
 error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
 sess = tf.Session()
@@ -170,7 +118,8 @@ error_train_ = []
 error_valid_ = []
 
 for n in range(max_epoch):
-    print('training epoch {0:d}'.format(n+1))
+    if n%100 == 0:
+        print('training epoch {0:d}'.format(n+1))
 
     for X_train_cur, y_train_cur, sl_train_cur in zip(X_train_batches, y_train_batches, sl_train_batches):
         sess.run(train_step, feed_dict={X: X_train_cur, y: y_train_cur, seq_length: sl_train_cur})
@@ -178,7 +127,8 @@ for n in range(max_epoch):
     error_train = sess.run(error, {X: X_train, y: y_train, seq_length: sl_train})
     error_valid = sess.run(error, {X: X_valid, y: y_valid, seq_length: sl_valid})
 
-    print('  train:{0:.3g}, valid:{1:.3g}'.format(error_train, error_valid))
+    if n%100 == 0:
+        print('  train:{0:.3g}, valid:{1:.3g}'.format(error_train, error_valid))
 
     error_train_ += [error_train]
     error_valid_ += [error_valid]
@@ -187,7 +137,11 @@ for n in range(max_epoch):
         break
 
 error_test = sess.run(error, {X: X_test, y: y_test, seq_length: sl_test})
+error_valid = sess.run(error, {X: X_test, y: y_test, seq_length: sl_test})
+error_train = sess.run(error, {X: X_test, y: y_test, seq_length: sl_test})
 print('-'*70)
+print('train error after epoch {0:d}: {1:.3f}'.format(n+1, error_train))
+print('validation error after epoch {0:d}: {1:.3f}'.format(n+1, error_valid))
 print('test error after epoch {0:d}: {1:.3f}'.format(n+1, error_test))
 
 sess.close()
